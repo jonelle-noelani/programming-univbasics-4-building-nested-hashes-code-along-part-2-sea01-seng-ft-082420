d843ba61d2ee39c351c058c1ab875f638fbac77b
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 09:30:30 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -54,7 +54,7 @@ def add_characters
 		    {:name => “Apothecary”}
         ]
    }
-
+end
    # Write code below that does the following programmatically:
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
