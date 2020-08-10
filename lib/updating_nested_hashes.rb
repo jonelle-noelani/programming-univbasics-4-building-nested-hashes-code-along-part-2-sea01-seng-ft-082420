0413bc06d9268938ca1f22dd0999657c4c523143
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 10:11:26 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -58,7 +58,7 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
    
-epic_tragedy[:additional_characters] => []
+epic_tragedy[:additional_characters] = []
 
    # After your code, the following line will return the altered hash
   epic_tragedy
